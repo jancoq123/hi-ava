96e37d52b24013490c9c0c069cf7ba3e80698ead
jancoq123 <49936106+jancoq123@users.noreply.github.com>
2020-07-02 04:05:46 +0700

    Add files via upload

---
+++ assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "Dena Kamal D.", quotes: "Whatever happens, keep breathing" },
+    { author: "Dena Kamal D", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "Dena Kamal D", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Dena Kamal D", quotes: "I love you." },
+    { author: "Dena Kamal D", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Dena Kamal D", quotes: "I love you." },
+    { author: "Dena Kamal D", quotes: "I'm sorry for loving you." },
+];
