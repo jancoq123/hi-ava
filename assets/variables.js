67cdf65ba63aa57b5187d51723016d72802867b3
jancoq123 <49936106+jancoq123@users.noreply.github.com>
2020-07-02 03:47:07 +0700

    Add files via upload

---
+++ assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["Mutii", "Babe", "Sayang", "Sweety", "Cinta", "Cutewitch","Sweetheart","Cantik"],
+  greetings: {
+    evening: "Good Evening",
+    afternoon: "Good Afternoon",
+    day: "Good Day and Have a Nice Day",
+    morning: "Good Morning ",
+    night: "Good Night and Have a Nice Day Later",
+  }
+};
